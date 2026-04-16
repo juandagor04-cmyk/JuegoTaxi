aeba10dc4402d600197ab70cdeec67e8d7e6cd18
Juan David <juandagor04@gmail.com>
2026-04-16 11:22:28 -0500

    4

--- src/Mapa.java
+++ src/Mapa.java
@@ -3,75 +3,125 @@ import java.util.Random;
 
 
 public class Mapa {
-    private int tipoMapa; //0=recta, 1=T, 2=+, 3=glorieta
-    private int[] carriles ={100, 200, 300};
+    private int tipoMapa; // 0= Recta horizontal, 1= Recta vertical, 2=T, 3=+, 4=glorieta
+    private int [] carriles = {150,250,350};
 
     private Random random = new Random();
+
+    //Dimension del mapa
+    private int anchoMapa , altoMapa;
+    private int offsetX = 0, offsetY = 0; //Centrar el mapa
+
+    //Bordes de la calle
+    private int calleLimiteSuperior, calleLimiteInferior, calleLimiteIzquierdo, calleLimiteDerecho;
+
+    //Constante Colores
+    private final Color ASFALTO = new Color(40,40,45);
+    private final Color LINEA_AMARILLA = new Color(255,220,0);
+    private final Color LINEA_BLANCA = new Color(255,255,255);
+    private final Color ANDEN = new Color (80,80,85);
+    private final Color BORDE_ANDEN =new Color(100,100,105);
+    private final Color CESPED = new Color (50,100,50);
+    private final Color TIERRA = new Color (101,67,33);
 
     public Mapa(){
         generarMapa();
     }
     public void generarMapa(){
-        tipoMapa = random.nextInt(4);
+        tipoMapa = random.nextInt(5); //5  Tipos Diefrentes
     }
-    public void dibujar(Graphics g, int ancho, int alto){
+    public void dibujar (Graphics g, int ancho, int alto){
+        anchoMapa= ancho;
+        altoMapa = alto;
 
-        //Fondo
-        g.setColor(Color.DARK_GRAY);
-        g.fillRect(0,0,ancho,alto);
-        switch (tipoMapa) {
+        //Fondo de tierra / cesped
+        g.setColor(TIERRA);
+        g.fillRect(0,0,ancho, alto);
+
+        g.setColor(CESPED);
+        for (int i = 0; i < 200; i++){
+            g.fillRect(random.nextInt(ancho), random.nextInt(alto),2,2);
+        }
+        switch (tipoMapa){
             case 0:
-                dibujarRecta(g, ancho, alto);
+                dibujarRectaHorizontal(g,ancho,alto);
                 break;
             case 1:
-                dibujarInterseccionT(g, ancho, alto);
+                dibujarRectaVertical(g,ancho,alto);
                 break;
             case 2:
-                dibujarInterseccionMas(g, ancho, alto);
+                dibujarInterseccionT(g,ancho,alto);
                 break;
             case 3:
-                dibujarGlorieta(g, ancho, alto);
+                dibujarInterseccionMas(g,ancho,alto);
+                break;
+            case 4:
+                dibujarGlorieta(g,ancho,alto);
                 break;
         }
     }
-    //Recta
-    private void dibujarRecta(Graphics g, int ancho, int alto){
-        g.setColor(Color.WHITE);
-        for (int x : carriles){
-            g.drawLine(x,0,x,alto);
+    //Recta Horizontal
+    private void dibujarRectaHorizontal(Graphics g, int ancho, int alto){
+        int centroY = alto /2;
+        int anchoCalle = 200;
+        int inicioCalle = centroY - anchoCalle/2;
+
+        //Calle Asfalto
+        g.setColor(ASFALTO);
+        g.fillRect(0,inicioCalle,ancho,anchoCalle);
+
+        //Linea de la calle
+        g.drawLine(0, inicioCalle + 5, ancho, inicioCalle + 5);
+        g.drawLine(0,inicioCalle + anchoCalle - 5,ancho,inicioCalle + anchoCalle - 5);
+
+        //Linea amarilla central discontinua
+        g.setColor(LINEA_AMARILLA);
+        ((Graphics2D)g).setStroke(new BasicStroke(3));
+        for (int x = 0; x < ancho; x += 40){
+            g.fillRect(x, centroY - 3, 20, 6);
         }
-    }
-    //Interseccion T
-    private void dibujarInterseccionT(Graphics g, int ancho, int alto){
-        g.setColor(Color.WHITE);
+        //Carriles
+        g.setColor(LINEA_BLANCA);
+        ((Graphics2D)g).setStroke(new BasicStroke(1));
+        for (int i=1; i<=2; i++){
+            int yCarril = inicioCalle + (anchoCalle /3)*i;
+            for (int x = 0; x < ancho; x +=30){
+                g.fillRect(x, yCarril - 1, 15, 2);
+            }
+        }
+        //Andenes
+        dibujarAnden(g,0,inicioCalle - 30, ancho, 30,true);
+        dibujarAnden(g,0,inicioCalle + anchoCalle, ancho, 30,false);
 
-        //Vertical
-        g.fillRect(180, 0, 40, alto);
+        //Establecer limites de la calle
+        calleLimiteSuperior = inicioCalle;
+        calleLimiteInferior = inicioCalle + anchoCalle;
+        calleLimiteIzquierdo = 0;
+        calleLimiteDerecho = ancho;
+    }
+    //Recta vertical
+    private void dibujarRectaVertical(Graphics g, int ancho, int alto){
+        int CentroX = ancho / 2;
+        int anchoCalle = 200;
+        int inicioCalle = CentroX - anchoCalle/2;
 
-        //Horinzontal
-        g.fillRect(0,0,ancho,40);
-    }
-    //Interseccion +
-    private void dibujarInterseccionMas (Graphics g, int ancho, int alto){
-        g.setColor(Color.WHITE);
-        g.fillRect(180,0,40,alto); //Vertical
-        g.fillRect(0, 180,ancho, 40); //Horinzontal
-    }
-    //Glorieta
-    private void dibujarGlorieta(Graphics g, int ancho, int alto){
-        //Calle Fondo
-        g.setColor(Color.DARK_GRAY);
-        g.fillRect(0,0,ancho,alto);
+        //Calle asfalto
+        g.setColor(ASFALTO);
+        g.fillRect(inicioCalle,0,anchoCalle, alto);
 
-        //Circulo
-        g.setColor(Color.WHITE);
-        g.drawOval(ancho/2 - 80, alto/2 - 80, 160, 160);
-    }
-    //Aleatorio
-    public int getCarrilALetatorio(){
-        return carriles [random.nextInt(carriles.length)];
-    }
-    public int []getCarriles (){
-        return carriles;
+        //Bordes blancos
+        g.setColor(LINEA_BLANCA);
+        ((Graphics2D)g).setStroke(new BasicStroke(2));
+        g.drawLine(inicioCalle + 5, 0, inicioCalle + 5, alto);
+        g.drawLine(inicioCalle + anchoCalle - 5, 0, inicioCalle + anchoCalle - 5, alto);
+
+        //Linea Amarilla cnetral discontinua
+        g.setColor(LINEA_AMARILLA);
+        ((Graphics2D)g).setStroke(new BasicStroke(3));
+        for (int y = 0; y < alto; y+= 40){
+            g.fillRect(centroX - 3, y, 6, 20);
+        }
+
+
     }
 }
